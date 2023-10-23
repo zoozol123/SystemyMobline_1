f31ce524aeb938d260d62ea965a05891af0b80ad
zozol123 <filiplazewski02@gmail.com>
2023-10-23 15:22:23 +0200

    Final commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button trueButton;
+    private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
+    private int currentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        nextButton = findViewById(R.id.next_button);
+        questionTextView = findViewById(R.id.question_text_view);
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswerCorrectness(true);
+            }
+        });
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswerCorrectness(false);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex = (currentIndex + 1) % questions.length;
+                setNextQuestion();
+            }
+        });
+        setNextQuestion();
+    }
+
+    private Question[] questions = new Question[] {
+            new Question(R.string.add, true),
+            new Question(R.string.multiply, true),
+            new Question(R.string.animal, true),
+            new Question(R.string.color, true),
+            new Question(R.string.q_activity, true)
+    };
+
+    private void checkAnswerCorrectness(boolean userAnswer) {
+        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+        int resultMessageId = 0;
+        if(userAnswer == correctAnswer) {
+            resultMessageId = R.string.correct_answer;
+        } else {
+            resultMessageId = R.string.incorrect_answer;
+        }
+        Toast.makeText(this,resultMessageId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void setNextQuestion() {
+        questionTextView.setText(questions[currentIndex].getQuestionId());
+    }
+}
